2ebf7292b66e9ba0f0a826f2c34761e176354148
Katarina Salcedo <ksalcedo04@gmail.com>
2021-10-10 15:25:51 -0700

    update functions

--- function.py
+++ function.py
@@ -3,6 +3,71 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import FunctionTransformer
 import numpy as np
+import pandas as pd
+
+def get_album_songs(uri_info):
+    uri = []
+    track = []
+    duration = []
+    explicit = []
+    track_number = []
+    one = sp.album_tracks(uri_info, limit=50, offset=0, market='US')
+    df1 = pd.DataFrame(one)
+    
+    for i, x in df1['items'].items():
+        uri.append(x['uri'])
+        track.append(x['name'])
+        duration.append(x['duration_ms'])
+        explicit.append(x['explicit'])
+        track_number.append(x['track_number'])
+    
+    df2 = pd.DataFrame({
+    'uri':uri,
+    'track':track,
+    'duration_ms':duration,
+    'explicit':explicit,
+    'track_number':track_number})
+    
+    return df2
+
+def get_track_info(df):
+    danceability = []
+    energy = []
+    key = []
+    loudness = []
+    speechiness = []
+    acousticness = []
+    instrumentalness = []
+    liveness = []
+    valence = []
+    tempo = []
+    
+    for i in df['uri']:
+        for x in sp.audio_features(tracks=[i]):
+            danceability.append(x['danceability'])
+            energy.append(x['energy'])
+            key.append(x['key'])
+            loudness.append(x['loudness'])
+            speechiness.append(x['speechiness'])
+            acousticness.append(x['acousticness'])
+            instrumentalness.append(x['instrumentalness'])
+            liveness.append(x['liveness'])
+            valence.append(x['valence'])
+            tempo.append(x['tempo'])
+            
+    df2 = pd.DataFrame({
+    'danceability':danceability,
+    'energy':energy,
+    'key':key,
+    'loudness':loudness,
+    'speechiness':speechiness,
+    'acousticness':acousticness,
+    'instrumentalness':instrumentalness,
+    'liveness':liveness,
+    'valence':valence,
+    'tempo':tempo})
+    
+    return df2
 
 def single_reg(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
@@ -22,7 +87,7 @@ transformer = FunctionTransformer(log_transform)
 
 
 def full_reg(model, X_train, X_test, y_train, y_test):
-    pipeline = Pipeline([('ss', StandardScaler()), ('transform', transformer), ('regressor', model)])
+    pipeline = Pipeline([('transform', transformer), ('ss', StandardScaler()), ('regressor', model)])
     
     X_train = X_train.values.reshape(-1,1)
     X_test = X_test.values.reshape(-1,1)
